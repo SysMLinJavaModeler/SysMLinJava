e376e9209ae8f118bdb4dd86b3d1110cd52cc9e8
YellowstoneBackpacker <54550824+YellowstoneBackpacker@users.noreply.github.com>
2022-11-13 17:56:10 -0600

    Delete ExampleModels/AIControlledDBSystem directory

--- ExampleModels/AIControlledDBSystem/dbssystem/actuators/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Actuator model element of the DBS System model. The DBSActuator
- * models/simulates a system component that is embedded in the patient's brain
- * and receives control signals from the DBSController. It translates these
- * control signals into very small power signals, injecting them into the
- * patients brain to reduce his tremor.
- */
-package dbssystem.actuators;
--- ExampleModels/AIControlledDBSystem/dbssystem/common/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Collection of model elements that are common to other packages of the DBS
- * System model.
- */
-package dbssystem.common;
