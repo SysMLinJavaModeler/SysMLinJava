89b8ba75b60c94bb5d8889f5b58172da5ac856b8
YellowstoneBackpacker <54550824+YellowstoneBackpacker@users.noreply.github.com>
2022-11-13 13:38:06 -0600

    Delete sysmlinjava/analysis/common directory

--- sysmlinjava/analysis/common/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains classes that are common to multiple analysis packages, including the
- * {@code UDPTransmitter} classes that are used by the constraint blocks in
- * executable models to transmit model data to the various displays that are
- * provided by the analysis packages..
- */
-package sysmlinjava.analysis.common;
